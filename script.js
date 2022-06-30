4077d0d1b8826a68f9b583fe9df1d21b03ee0a4c
kainpets <pawelstepniak22@gmail.com>
2022-06-30 22:40:39 +0200

    checks for win and draw

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const xClass = "x";
 const oClass = "o";
 const fields = document.querySelectorAll("[data-cell]");
-const winningConditions = [
+const winMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.getElementById("winningMessage");
+const winConditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -10,7 +14,7 @@ const winningConditions = [
   [2, 5, 8],
   [0, 4, 8],
   [6, 4, 2],
-]
+];
 let xTurn;
 
 fields.forEach((field) => {
@@ -19,15 +23,45 @@ fields.forEach((field) => {
 
 function main(event) {
   const field = event.target; // whatever was clicked
-  const currentClass = xTurn ? oClass : xClass; // appropriate class to apply
-  placeMark(field, currentClass);
-  switchTurns();
+  const currentPlayer = xTurn ? oClass : xClass; // appropriate class to apply
+  placeMark(field, currentPlayer);
+  if (checkWin(currentPlayer)) {
+    //checks for winner by looking at current player
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    switchTurns();
+  }
 }
 
-function placeMark(field, currentClass) {
-  field.classList.add(currentClass);
+function endGame(draw) {
+  if (draw) {
+    winMessageTextElement.innerText = "Draw!";
+  } else {
+    winMessageTextElement.innerText = `${xTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessage.classList.add("show");
+}
+
+function isDraw() {
+  return [...fields].every(field => {
+    return field.classList.contains(xClass || field.classList.contains(oClass))
+  })
+}
+
+function placeMark(field, currentPlayer) {
+  field.classList.add(currentPlayer);
 }
 
 function switchTurns() {
   xTurn = !xTurn;
+}
+
+function checkWin(currentPlayer) {
+  return winConditions.some((combination) => {
+    return combination.every((index) => {
+      return fields[index].classList.contains(currentPlayer);
+    });
+  });
 }
